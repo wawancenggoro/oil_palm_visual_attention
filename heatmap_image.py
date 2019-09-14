17038d0a9d5d27ba5ce097dbcb1d71be73b2d968
devTeaa <devtea@protonmail.com>
2019-09-14 09:29:59 +0000

    draft heatmap_image

---
+++ heatmap_image.py
@@ -0,0 +1,106 @@
+import os
+import re
+import sys
+import numpy as np
+import torch
+import torchvision.utils as vutils
+import shutil
+from PIL import Image
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+for foldername in sorted(os.listdir('attention_image/result')):
+    for filename in os.listdir(f'attention_image/result/{foldername}'):
+        image_name = filename.split('.')[0]
+
+        # input_image = Image.open(f'attention_image/result/{foldername}/{filename}').convert('L')
+        # cm_hot = mpl.cm.get_cmap('hot')
+        # im = np.array(input_image)
+        # im = cm_hot(im)
+        # im = np.uint8(im * 255)
+        # im = Image.fromarray(im)
+        # im.save(f"attention_image/result/{foldername}/{image_name}-jet.jpg")
+
+        input_image = mpimg.imread(f'attention_image/result/{foldername}/{filename}')
+        im = np.dot(input_image[:,:,:3], [0.229, 0.587, 0.114])
+        f = plt.figure(figsize=(4, 4), dpi=128)
+        plt.axis('off')
+        plt.imshow(im, cmap='hot')
+        plt.savefig(f'attention_image/result/{foldername}/{image_name}-jet.jpg', dpi=f.dpi)
+        plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # input_image.load()
+        # numpy_image = np.asarray(input_image, dtype='int32')
+
+        # cmap = plt.get_cmap('jet')
+        
+        # rgba_image = cmap(numpy_image)
+        # rgba_image.savefig(f"attention_image/result{foldername}/{filename.split('.')[0]}-jet.jpg")
+        # break
+        # Image.fromarray((cmap_image[:, :, :3] * 255).astype(np.uint8)).save(f"attention_image/result/{foldername}/{filename.split('')[0]}-jet.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # if folder_pattern.match(foldername):
+        # numpy_name = foldername.split('_')[2]
+
+        # title_pattern = re.compile(f'^{numpy_name}.+\.npy')
+        # org_pattern = re.compile(f'^org_{numpy_name}\.npy')
+
+        # if os.path.exists(f'attention_image/result/{foldername}'):
+            # shutil.rmtree(f'attention_image/result/{foldername}')
+        # os.makedirs(f'attention_image/result/{foldername}')
+
+        # for filename in os.listdir(f'attention_image/{foldername}'):
+            # if title_pattern.match(filename):
+                # numpy_attention = np.load(f'attention_image/{foldername}/{filename}')
+                # attention = torch.from_numpy(numpy_attention).float().to('cuda:0')
+                # # attention_global = torch.from_numpy(numpy_attention).float().to('cuda:0').sum(0)
+
+                # # max_global = attention_global.max().cpu().data.numpy()
+                # # image_global = attention_global / (max_global * 1 if max_global != 0 else 1) * 255
+                # # vutils.save_image(image_global, f'attention_image/result/{title_arg}_global.jpg', normalize=True)
+
+                # for i in range(attention.size()[0]):
+                    # max_local = attention[i].max().cpu().data.numpy()
+                    # image_local = attention[i] / (max_local * 1 if max_local != 0 else 1) * 255
+                    # image_name = f"{filename.split('_')[1].split('.')[0]}_{i}.jpg"
+                    # vutils.save_image(image_local, f'attention_image/result/{foldername}/{image_name}', normalize=True)
+            # elif org_pattern.match(filename):
+                # numpy_image = np.load(f'attention_image/{foldername}/{filename}')
+                # org_image = torch.from_numpy(numpy_image).float().to('cuda:0')
+                # image_name = f"0_org_{filename.split('_')[1].split('.')[0]}.jpg"
+                # vutils.save_image(org_image, f'attention_image/result/{foldername}/{image_name}', normalize=True)
